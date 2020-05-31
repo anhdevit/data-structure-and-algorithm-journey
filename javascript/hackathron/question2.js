5903590795ffb77d4fabff311b2ae62846b6087c
AnhDevit <anhdevit@gmail.com>
2020-05-31 11:33:32 +0700

    Linked List

--- javascript/hackathron/question2.js
+++ javascript/hackathron/question2.js
@@ -21,3 +21,190 @@ rotate 3 steps to the right: 0->1->2->NULL
 rotate 4 steps to the right: 2->0->1->NULL
 
 */
+
+console.log("LinkedList", LinkedList);
+
+function question2(linkedList, k) {
+  linkedList.linkedList();
+
+  for (let i = 0; i < k; i++) {
+      
+  }
+}
+
+// creating an object for the
+// Linkedlist class
+
+class Node {
+  // constructor
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(props) {
+    this.head = null;
+    this.size = 0;
+  }
+
+  size() {
+    return this.size;
+  }
+
+  isEmpty() {
+    return this.size == 0;
+  }
+
+  pushFront(item) {}
+
+  popFront() {}
+
+  printList() {
+    var curr = this.head;
+    var str = "";
+    while (curr) {
+      str += curr.element + " ";
+      curr = curr.next;
+    }
+    console.log(str);
+  }
+
+  pushBack(element) {
+    // creates a new node
+    var node = new Node(element);
+
+    // to store current node
+    var current;
+
+    // if list is Empty add the
+    // element and make it head
+    if (this.head == null) this.head = node;
+    else {
+      current = this.head;
+
+      // iterate to the end of the
+      // list
+      while (current.next) {
+        current = current.next;
+      }
+
+      // add node
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  popBack() {}
+
+  front() {
+    return this.head;
+  }
+
+  back() {}
+
+  add(element) {
+    // creates a new node
+    var node = new Node(element);
+
+    // to store current node
+    var current;
+
+    // if list is Empty add the
+    // element and make it head
+    if (this.head == null) this.head = node;
+    else {
+      current = this.head;
+
+      // iterate to the end of the
+      // list
+      while (current.next) {
+        current = current.next;
+      }
+
+      // add node
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  insert(element, index) {
+    if (index > 0 && index > this.size) return false;
+    else {
+      // creates a new node
+      var node = new Node(element);
+      var curr, prev;
+
+      curr = this.head;
+
+      // add the element to the
+      // first index
+      if (index == 0) {
+        node.next = this.head;
+        this.head = node;
+      } else {
+        curr = this.head;
+        var it = 0;
+
+        // iterate over the list to find
+        // the position to insert
+        while (it < index) {
+          it++;
+          prev = curr;
+          curr = curr.next;
+        }
+
+        // adding an element
+        node.next = curr;
+        prev.next = node;
+      }
+      this.size++;
+    }
+  }
+
+  removeAt(element) {
+    var current = this.head;
+    var prev = null;
+
+    // iterate over the list
+    while (current != null) {
+      // comparing element with current
+      // element if found then remove the
+      // and return true
+      if (current.element === element) {
+        if (prev == null) {
+          this.head = current.next;
+        } else {
+          prev.next = current.next;
+        }
+        this.size--;
+        return current.element;
+      }
+      prev = current;
+      current = current.next;
+    }
+    return -1;
+  }
+}
+
+var ll1 = new LinkedList();
+
+ll1.add(1);
+ll1.add(2);
+ll1.add(3);
+ll1.add(4);
+ll1.add(5);
+
+const testcase1 = question2(ll1, 2);
+console.log("testcase1", testcase1);
+
+var ll2 = new LinkedList();
+
+ll2.add(1);
+ll2.add(2);
+ll2.add(3);
+ll2.add(4);
+ll2.add(5);
+const testcase2 = question2(ll2, 4);
+console.log("testcase1", testcase2);
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -24,6 +24,16 @@ class LinkedList {
 
   popFront() {}
 
+  printList() {
+    var curr = this.head;
+    var str = "";
+    while (curr) {
+      str += curr.element + " ";
+      curr = curr.next;
+    }
+    console.log(str);
+  }
+
   pushBack(element) {
     // creates a new node
     var node = new Node(element);
@@ -55,8 +65,31 @@ class LinkedList {
     return this.head;
   }
 
-  back() {
-    
+  back() {}
+
+  add(element) {
+    // creates a new node
+    var node = new Node(element);
+
+    // to store current node
+    var current;
+
+    // if list is Empty add the
+    // element and make it head
+    if (this.head == null) this.head = node;
+    else {
+      current = this.head;
+
+      // iterate to the end of the
+      // list
+      while (current.next) {
+        current = current.next;
+      }
+
+      // add node
+      current.next = node;
+    }
+    this.size++;
   }
 
   insert(element, index) {
@@ -117,3 +150,15 @@ class LinkedList {
     return -1;
   }
 }
+
+export default LinkedList
+
+// creating an object for the
+// Linkedlist class
+var ll = new LinkedList();
+
+ll.add(10);
+ll.add(20);
+
+// prints 10
+ll.printList();
