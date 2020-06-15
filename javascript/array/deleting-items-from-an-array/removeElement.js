007a3735742deeffa6329d84feadbc227c201ffa
AnhDevit <anhdevit@gmail.com>
2020-06-16 06:40:19 +0700

    removeDuplicates

---
+++ javascript/array/deleting-items-from-an-array/removeElement.js
@@ -0,0 +1,9 @@
+var removeDuplicates = function (nums) {
+    var i = 0;
+    nums.forEach(function (elem) {
+        if (elem !== nums[i]) {
+            nums[++i] = elem;
+        }
+    });
+    return nums.length && i + 1;
+};
